69ad2e4d5be8deb63004594759957860abf0ffbd
Jacob McKinney <jmckinney@imageinabox.com>
2020-11-24 11:30:56 -0600

    Added placeholder components and pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
       <Layout>
-        <h1>Heading 1</h1>
+        <h1>Home</h1>
         <h2>Heading 2</h2>
         <h3>Heading 3</h3>
         <h4>Heading 4</h4>

25e9eb6106e3f3f8edc2d87ad136260f0223cc80
Jacob McKinney <jmckinney@imageinabox.com>
2020-12-01 21:47:35 -0600

    Updated banner component and increased size

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,10 +16,8 @@ export default function Banner() {
 		 }
 	`)
 
-	const Background = styled(BackgroundImage)`
-		background: linear-gradient(93.88deg, #000000 0%, rgba(42, 42, 42, 0.834768) 48.27%, rgba(67, 67, 67, 0.736985) 66.49%, rgba(126, 126, 126, 0.506307) 86.4%, rgba(255, 255, 255, 0) 100%) center center;
-		background-size: cover;
-		opacity: 1 !important;
+	const BackgroundGradient = styled.div`
+		background: linear-gradient(93.88deg, #000000 0%, rgba(42, 42, 42, 0.834768) 48.27%, rgba(67, 67, 67, 0.736985) 66.49%, rgba(126, 126, 126, 0.506307) 86.4%, rgba(255, 255, 255, 0) 100%);
 	`;
 
 	const Content = styled.div`
@@ -29,39 +27,39 @@ export default function Banner() {
 		color: rgba(252, 252, 252, 0.8);
 	`;
 
-	const Heading = styled.div`
-		margin-bottom: 15px;
-		font-family: Roboto;
-		font-style: normal;
+	const Heading = styled.h2`
+		margin-bottom: 20px;
 		font-weight: 400;
-		font-size: 36px;
+		font-size: 45px;
 		line-height: 42px;
 		letter-spacing: 1px;
 	`;
 
 	const Copy = styled.div`
-		max-width: 480px;
-		font-size: 22px;
+		max-width: 625px;
+		font-size: 28px;
 		font-weight: 300;
-		letter-spacing: 1px;
-		line-height: 25px;
+		letter-spacing: 1.5px;
+		line-height: 34px;
 	`;
 
 	return (
-		<Background
+		<BackgroundImage
 			tag={"section"}
 			fluid={data.file.childImageSharp.fluid}
 		>
-			<Content>
-				<Heading>Loved | Forgiven | Giving</Heading>
-				<Copy>
-					"This is how we know what love is: Jesus Christ
-					laid down his life for us. And we ought to lay down
-					our lives for our brothers and sisters."
-					<br />
-					- 1 John 3:16
-				</Copy>
-			</Content>
-		</Background>
+			<BackgroundGradient>
+				<Content>
+					<Heading>Loved | Forgiven | Giving</Heading>
+					<Copy>
+						"This is how we know what love is: Jesus Christ
+						laid down his life for us. And we ought to lay down
+						our lives for our brothers and sisters."
+						<br />
+						- 1 John 3:16
+					</Copy>
+				</Content>
+			</BackgroundGradient>
+		</BackgroundImage>
 	)
 }

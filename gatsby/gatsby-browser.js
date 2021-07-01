be599b65083bd9b62a97c5a0fb76d6da8582885f
Jacob McKinney <jmckinney@imageinabox.com>
2021-07-01 09:29:42 -0500

    Updated Search component with some styling

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,24 +1,10 @@
-const addStylesheet = url => {
-	const stylesheet = document.createElement("link")
-	stylesheet.rel = "stylesheet"
-	stylesheet.href = url
-	document.head.appendChild(stylesheet)
-}
-
-const addScript = (url, footer = true ) => {
-	const script = document.createElement("script")
-	script.src = url
-	script.async = true
-	if ( footer ) {
-		document.body.appendChild(script)
-	} else {
-		document.head.appendChild(script)
+export const onClientEntry = () => {
+	window.onload = () => {
+		const algoliaStyleSheet = document.createElement("link")
+		algoliaStyleSheet.rel = "stylesheet"
+		algoliaStyleSheet.href = "https://cdn.jsdelivr.net/npm/instantsearch.css@7.4.5/themes/satellite-min.css"
+		algoliaStyleSheet.integrity = "sha256-TehzF/2QvNKhGQrrNpoOb2Ck4iGZ1J/DI4pkd2oUsBc="
+		algoliaStyleSheet.crossOrigin = "anonymous"
+		document.head.appendChild(algoliaStyleSheet)
 	}
 }
-
-// export const onClientEntry = () => {
-// 	window.onload = () => {
-// 		addStylesheet("https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css")
-// 		addScript("https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js", false)
-// 	}
-// }
--- gatsby/src/components/Search.js
+++ gatsby/src/components/Search.js
@@ -7,64 +7,96 @@ import styled from "styled-components"
 // Check if window is defined (so if in the browser or in node.js).
 const isBrowser = typeof window !== "undefined"
 
+const SearchButton = styled.button`
+  background: transparent;
+  padding-top: 3px;
+  cursor: pointer;
+  border: none; 
+`
+
+const SearchPanel = styled.div`
+  display: none;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: rgba(0,0,0,0.6);
+  z-index: 10000;
+  
+  .search-container {
+	position:relative;
+	background: #fff;
+	width: 100%;
+	max-width: 600px;
+	border-radius: 15px;
+	overflow: hidden;
+	padding: 50px 25px;
+	
+	.close {
+	  position: absolute;
+	  top: 5px;
+	  right: 20px;
+	  opacity: 0.5;
+	  background: none;
+	  border: none;
+	}
+  }
+  
+  .ais-SearchBox {
+	form {
+	  input {
+		width: calc(100% - 50px);	  
+		height: 50px;
+	  }
+	  button {
+		width: 50px;
+		height: 50px;
+	  }
+	}
+  }
+  .ais-Hits {
+	max-height: 500px;
+	overflow: scroll;
+	
+	.ais-Hits-item {
+	  padding: 0.5rem;
+	  box-shadow: none;
+	  font-size: 24px;
+	}
+  }
+  
+  &.open {
+	display: flex;
+  }
+`
+
 export default function Search() {
 	const searchClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY)
 
 	let searchPanel = ''
 
-	if ( isBrowser ) {
-		searchPanel = document.getElementById("search-panel")
-	}
+	console.log( isBrowser )
+
+	// if ( isBrowser ) {
+		searchPanel = document.getElementById("search-overlay")
+	// }
+
+	console.log( searchPanel )
 
 	const openSearchPanel = () => {
-		searchPanel.classList.add('open')
-		console.log("search panel opened!")
+		if ( searchPanel !== '' && searchPanel !== null ) {
+			searchPanel.classList.add('open')
+		}
 	}
 
 	const closeSearchPanel = () => {
 		searchPanel.classList.remove('open')
 	}
 
-	const SearchButton = styled.button`
-	  background: transparent;
-	  padding-top: 3px;
-      cursor: pointer;
-      border: none;
-	`
-
-	const SearchPanel = styled.div`
-	  display: none;
-	  justify-content: center;
-	  align-items: center;
-	  position: absolute;
-	  top: 0;
-	  left: 0;
-	  height: 100%;
-	  width: 100%;
-	  background: rgba(0,0,0,0.6);
-	  z-index: 10000;
-	  
-	  .ais-SearchBox {
-	  	width: 90%;
-	  	max-width: 600px;
-	  	
-	  	form {
-	  	  input {
-	  		width: calc(100% - 50px);	  
-	  	  	height: 50px;
-	  	  }
-	  	  button {
-	  	    width: 50px;
-	  	    height: 50px;
-	  	  }
-	  	}
-	  }
-	  .ais-Hits {}
-	  
-	  &.open {
-	  	display: flex;
-	  }
-	`
+	const Hit = ({hit}) => <a href={hit.url}>{hit.title}</a>
 
 	return (
 		<div>
@@ -72,11 +104,16 @@ export default function Search() {
 				<FaSearch />
 				<span className={"sr-only"}>Open Search</span>
 			</SearchButton>
-			<SearchPanel id={"search-panel"}>
-				<InstantSearch searchClient={searchClient} indexName={"test"}>
-					<SearchBox />
-					<Hits />
-				</InstantSearch>
+			<SearchPanel id={"search-overlay"}>
+				<div className={"search-container"}>
+					<button className={"close"} aria-label="Close search panel" type="button" onClick={closeSearchPanel}>
+						<span aria-hidden="true">Close <span style={{fontSize: 25 + 'px'}}>&times;</span></span>
+					</button>
+					<InstantSearch searchClient={searchClient} indexName={"netlify_ae06d06d-e53b-4e86-a343-0285a6856e91_main_all"}>
+						<SearchBox />
+						<Hits hitComponent={Hit} />
+					</InstantSearch>
+				</div>
 			</SearchPanel>
 		</div>
 	)

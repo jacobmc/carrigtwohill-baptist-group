9cbc75597422e46d98c0132cbcb4625f546c17e1
Jacob McKinney <jmckinney@imageinabox.com>
2022-01-29 21:01:38 -0600

    Removed unnecessary method and action attributes from contact form

--- gatsby/src/components/ContactForm.js
+++ gatsby/src/components/ContactForm.js
@@ -138,7 +138,7 @@ export default function ContactForm() {
 	return (
 		<div>
 			<Messages id={'messages'} />
-			<Form id={'contact-form'} name={"Contact"} method={"post"} netlify data-netlify-recaptcha={"true"} action={"/contact"} onSubmit={submitForm}>
+			<Form id={'contact-form'} name={"Contact"} netlify data-netlify-recaptcha={"true"} onSubmit={submitForm}>
 				<input type={"hidden"} name={"form-name"} value={"Contact"} />
 
 				<div className={"field"}>

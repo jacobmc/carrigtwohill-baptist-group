2893c9bb349d492d1240a17906f041c352233bfc
Jacob McKinney <jmckinney@imageinabox.com>
2022-01-31 22:10:56 -0600

    Fixed styled-component errors on Card.js

--- gatsby/src/components/Card.js
+++ gatsby/src/components/Card.js
@@ -8,6 +8,61 @@ import {
 } from "react-icons/hi"
 import styled from "styled-components"
 
+const Container = styled.div`
+    position: relative;
+    max-width: 435px;
+    min-height: 292px;
+    background: #fcfcfc;
+    padding: 36px 21px 18px;
+    box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.15);
+    border-radius: 8px;
+  `
+
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+  `
+
+const IconContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    margin-right: 13px;
+    background: ${props => props.color ? props.color : "#FFB20F"};
+    color: #fcfcfc;
+    font-size: 30px;
+    border-radius: 50%;
+  `
+
+const Heading = styled.h2`
+    margin: 0;
+    font-size: 35px;
+    line-height: 40px;
+  `
+
+const Title = styled.h3`
+    margin: 20px 0 6px;
+    font-size: 28px;
+    line-height: 32px;
+  `
+
+const Description = styled.p`
+    color: rgba(38, 35, 34, 0.7);
+    font-size: 22px;
+    line-height: 24px;
+    margin: 0;
+  `
+
+const Links = styled.div`
+    position: absolute;
+    bottom: 18px;
+    right: 21px;
+    font-size: 18px;
+    line-height: 20px;
+  `
+
 export default function Card(props) {
   const { type, title, description } = props
   let iconBackground = null
@@ -51,69 +106,12 @@ export default function Card(props) {
     }
   }
 
-  const Container = styled.div`
-    position: relative;
-    max-width: 435px;
-    min-height: 292px;
-    background: #fcfcfc;
-    padding: 36px 21px 18px;
-    box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.15);
-    border-radius: 8px;
-  `
-
-  const Header = styled.header`
-    display: flex;
-    align-items: center;
-  `
-
-  const IconContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50px;
-    height: 50px;
-    margin-right: 13px;
-    background: ${iconBackground};
-    color: #fcfcfc;
-    font-size: 30px;
-    border-radius: 50%;
-  `
-
-  const Heading = styled.h2`
-    margin: 0;
-    font-size: 35px;
-    line-height: 40px;
-  `
-
-  const Title = styled.h3`
-    margin: 20px 0 6px;
-    font-size: 28px;
-    line-height: 32px;
-  `
-
-  const Description = styled.p`
-    color: rgba(38, 35, 34, 0.7);
-    font-size: 22px;
-    line-height: 24px;
-    margin: 0;
-  `
-
-  const Links = styled.div`
-    position: absolute;
-    bottom: 18px;
-    right: 21px;
-    font-size: 18px;
-    line-height: 20px;
-  `
-
   // TODO Set up links
-
-  // TODO fix styled component errors
 
   return (
     <Container>
       <Header>
-        <IconContainer>{renderIcon(type)}</IconContainer>
+        <IconContainer color={iconBackground}>{renderIcon(type)}</IconContainer>
         <Heading>{headingText}</Heading>
       </Header>
       <section>
